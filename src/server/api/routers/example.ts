3725848a3e19ed622865b7578849d87af9b6c72d
richard523 <ricgat523@gmail.com>
2023-09-23 21:01:54 -0500

    finishedish

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -34,14 +34,49 @@ export const exampleRouter = createTRPCRouter({
 
   // External API Call Procedure
   
-  fetchExternalData: publicProcedure
-  .input(z.object({ apiUrl: z.string() }))
-  .query(async ({ input }) => {
-    try {
-      const { apiUrl } = input; // Destructure the apiUrl from input
+  // fetchExternalData: publicProcedure
+  // .input(z.object({ apiUrl: z.string() }))
+  // .query(async ({ input }) => {
+  //   try {
+  //     const { apiUrl } = input; // Destructure the apiUrl from input
 
-      // Make an external API call using fetch
-      const response = await fetch(apiUrl, {
+  //     // Make an external API call using fetch
+  //     const response = await fetch(apiUrl, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         'x-api-key': env.ANTHROPIC_API_KEY,
+  //         'anthropic-version': '2023-06-01',
+  //       },
+  //       body: JSON.stringify({
+  //         model: 'claude-v2',
+  //         max_tokens_to_sample: 300,
+  //         temperature: 0.9,
+  //         stream: true,
+  //       }),
+  //     });
+
+  //     // Check if the response status is OK
+  //     if (!response.ok) {
+  //       throw new Error('External API request failed');
+  //     }
+
+  //     // Convert the response into a friendly text-stream
+  //     const stream = AnthropicStream(response);
+
+  //     // Respond with the stream
+  //     return new StreamingTextResponse(stream);
+  //   } catch (error) {
+  //     // Handle errors here
+  //     throw new Error("Failed to fetch external data");
+  //   }
+  // }),
+
+  anthropicComplete: protectedProcedure
+    .input(z.object({ prompt: z.string() }))
+    .query(async ({ input }) => {
+      // Make a request to the Anthropic API
+      const response = await fetch('https://api.anthropic.com/v1/complete', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -49,26 +84,19 @@ export const exampleRouter = createTRPCRouter({
           'anthropic-version': '2023-06-01',
         },
         body: JSON.stringify({
+          prompt: input.prompt,
           model: 'claude-v2',
           max_tokens_to_sample: 300,
           temperature: 0.9,
           stream: true,
         }),
       });
-
-      // Check if the response status is OK
-      if (!response.ok) {
-        throw new Error('External API request failed');
-      }
-
-      // Convert the response into a friendly text-stream
+      
+      // Convert the response into a friendly text-stream (you may need to define AnthropicStream)
       const stream = AnthropicStream(response);
+      console.log(response)
+      // Return the stream or handle it as needed
+      return stream;
+    }),
 
-      // Respond with the stream
-      return new StreamingTextResponse(stream);
-    } catch (error) {
-      // Handle errors here
-      throw new Error("Failed to fetch external data");
-    }
-  }),
 });
